e733ed3def065640d1ee6e5b6383a8604f6aa22e
trantienvn <tientn2005@gmail.com>
2024-12-06 20:13:01 +0700

    add api

--- app/api/schedule/route.ts
+++ app/api/schedule/route.ts
@@ -18,7 +18,7 @@ const client = wrapper(
 );
 const responseHeaders = {
   "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "https://lichhoc.wuaze.com", // Allow all origins
+  "Access-Control-Allow-Origin": "*", // Allow all origins
   "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // Allow methods
   "Access-Control-Allow-Headers": "Content-Type, Authorization", // Allow specific headers
